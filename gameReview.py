7a848f1b3bbb3849e2bc82b719244ada309215dd
carlylemorris1 <carlylemorris@tamu.edu>
2023-11-22 16:28:20 -0600

    added support for chess.com API

--- gameReview.py
+++ gameReview.py
@@ -5,32 +5,50 @@ import chess.engine
 from io import StringIO
 from matplotlib import pyplot as plt
 from math import log
+import requests
+import datetime
+#Chess.com username for automated retrival of previous game.
+USERNAME = ""
 
 ENGINE = "./tools/stockfish"
 ENGINE_DEPTH = 22 # engine analysis depth, go lower if you dont like waiting
 CHART_DPI = 600 # crips charts only
 MATE_SCORE = 100000
 
+def getLastGame(user: str):
+        now = datetime.datetime.now()
+        current_year = now.year
+        current_month = now.month
+        resp = requests.get(f"https://api.chess.com/pub/player/{user}/games/{current_year}/{current_month}",headers={"User-Agent":"carlylemorris@gmail.com"})
+        game = resp.json()["games"][-1]
+        return game["pgn"], game["white"]["username"].lower() == user.lower()
+
+
 def scale(x):
     return log(abs(x/1000) + 1) * (-1 if x<0 else 1)
 
 def main():
-    if len(sys.argv) > 3:
-        print("\nUsage: python3 gameReview.py [w/b] '[game]'\nMake sure expored game is quoted\n")
-        quit()
-    elif len(sys.argv) == 3:
-        uicolor = sys.argv[1]
-        sanGame = sys.argv[2]
-    else:
-        uicolor = input("Enter Your Color [w/b]: ")
-        sanGame = ""
-        temp = input("Enter PGN Game: ")
-        while(len(sanGame) < 3 or sanGame[-3:] != "\n\n\n"):
-            sanGame += temp + "\n"
-            temp = input()
+    if USERNAME == "":
+        if len(sys.argv) > 3:
+            print("\nUsage: python3 gameReview.py [w/b] '[game]'\nMake sure expored game is quoted\n")
+            quit()
 
-        
-    color = uicolor != "b"
+        elif len(sys.argv) == 3:
+            uicolor = sys.argv[1]
+            sanGame = sys.argv[2]
+
+        else:
+            uicolor = input("Enter Your Color [w/b]: ")
+            sanGame = ""
+            temp = input("Enter PGN Game: ")
+            while(len(sanGame) < 3 or sanGame[-3:] != "\n\n\n"):
+                sanGame += temp + "\n"
+                temp = input()
+
+            color = uicolor != "b"
+    else:
+        sanGame,color = getLastGame(USERNAME)
+    
     pgn = StringIO(sanGame)
     game = chess.pgn.read_game(pgn)
      
@@ -75,7 +93,7 @@ def main():
         print(f"Move {moveNum+md:3} {delta:10} ({premoves[moveNum]} to {postmoves[moveNum]})")
    
     finiteDeltas = [d for d in deltas if d < MATE_SCORE/10]
-    n = len(finitieDeltas)
+    n = len(finiteDeltas)
     mean = sum(finiteDeltas)/n
     std = (sum([(x-mean)**2 for x in finiteDeltas])/n)**0.5
 
